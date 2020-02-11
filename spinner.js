d9597038ec4199c18eb5018677cca45d815f28ed
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-02-11 12:18:03 -0500

    added spinner fn

---
+++ spinner.js
@@ -0,0 +1,10 @@
+const arr = ["|  ", "/  ", "-  ", "\\  "];
+let time = 200;
+for (const elem of arr) {
+  setTimeout(() => {
+    process.stdout.write("\r" + elem);
+  }, time);
+  time += 200; 
+  // <= 1s delay to make it noticeable. Can dial it down later.
+}
+
